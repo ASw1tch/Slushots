6bf9ea0d952843852abfe415ee297698a7b2224c
Anatolii Petrov <hello@aswitch.me>
2024-01-07 17:36:43 +0100

    Redisign WelcomeVC and added yandex music button

--- slushots/ViewControllers/YandexLoginViewController.swift
+++ slushots/ViewControllers/YandexLoginViewController.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import UIKit
+
+class YandexLoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
